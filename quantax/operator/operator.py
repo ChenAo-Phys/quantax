6cbc2256cabb7a7a533550e3617f8d8534173502
Ao Chen <chenao@adachi.physik.uni-augsburg.de>
2024-11-07 13:54:59 +0100

    apply operator by jax operations

--- quantax/operator/operator.py
+++ quantax/operator/operator.py
@@ -1,26 +1,105 @@
 from __future__ import annotations
 from typing import Optional, Tuple, Union
 from numbers import Number
+from functools import partial
 import numpy as np
 import jax
 import jax.numpy as jnp
 from jax.ops import segment_sum
 from jax.experimental.multihost_utils import process_allgather
+import equinox as eqx
 from quspin.operators import hamiltonian
 
 from ..state import State, DenseState
 from ..sampler import Samples
 from ..symmetry import Symmetry, Identity
 from ..utils import (
-    array_to_ints,
-    ints_to_array,
     get_global_sharding,
+    local_to_replicate,
     to_global_array,
     global_to_local,
     local_to_global,
     to_replicate_numpy,
 )
-from ..global_defs import get_default_dtype
+from ..global_defs import get_sites, get_default_dtype
+
+
+def _apply_site_operator(
+    x: jax.Array, opstr: str, J: jax.Array, idx: jax.Array
+) -> Tuple[jax.Array, jax.Array]:
+    # diagonal
+    if opstr == "I":
+        return x, J
+
+    if opstr == "n":
+        return x, jnp.where(x[idx] > 0, J, 0)
+
+    if opstr == "z":
+        return x, J * x[idx] / 2
+
+    # off-diagonal
+    if get_sites().is_fermion:
+        num_fermion = jnp.cumulative_sum(x[::-1] > 0, include_initial=True)[::-1]
+        J = jnp.where(num_fermion[idx + 1] % 2 == 0, J, -J)
+    elif opstr in ("x", "y"):
+        J /= 2
+
+    if opstr == "+":
+        J = jnp.where(x[idx] < 0, J, jnp.nan)
+        return x.at[idx].set(1), J
+
+    if opstr == "-":
+        J = jnp.where(x[idx] > 0, J, jnp.nan)
+        return x.at[idx].set(-1), J
+
+    if opstr == "x":
+        return x.at[idx].mul(-1), J
+
+    if opstr == "y":
+        J *= 1j * x[idx]
+        if get_sites().is_fermion:
+            J *= -1  # conventional sign difference
+        return x.at[idx].mul(-1), J
+
+
+@eqx.filter_jit
+@partial(jax.vmap, in_axes=(0, None))
+def _apply_diag(x, jax_op_list):
+    Hz = 0
+    apply_fn = jax.vmap(_apply_site_operator, in_axes=(None, None, 0, 0))
+
+    for opstr, J, index in jax_op_list:
+        if all(op in ("I", "n", "z") for op in opstr):
+            for op, idx in zip(opstr, index.T):
+                _, J = apply_fn(x, op, J, idx)
+            Hz += jnp.sum(J)
+
+    return Hz
+
+
+@eqx.filter_jit
+@partial(jax.vmap, in_axes=(0, None))
+def _apply_off_diag(x, jax_op_list):
+    out = dict()
+    apply_fn = jax.vmap(_apply_site_operator, in_axes=(0, None, 0, 0))
+
+    for opstr, J, index in jax_op_list:
+        nflips = sum(1 for s in opstr if s not in ("I", "n", "z"))
+        if nflips > 0:
+            x_conn = jnp.repeat(x[None, :], J.size, axis=0)
+            for op, idx in zip(reversed(opstr), reversed(index.T)):
+                x_conn, J = apply_fn(x_conn, op, J, idx)
+
+            if nflips in out:
+                out[nflips][0].append(x_conn)
+                out[nflips][1].append(J)
+            else:
+                out[nflips] = [[x_conn], [J]]
+
+    for nflips, (x_conn, J) in out.items():
+        out[nflips] = [jnp.concatenate(x_conn), jnp.concatenate(J)]
+
+    return out
 
 
 class Operator:
@@ -46,6 +125,7 @@ class Operator:
                     the site index that operators act on
         """
         self._op_list = op_list
+        self._jax_op_list = None
         self._quspin_op = dict()
         self._connectivity = None
 
@@ -53,6 +133,25 @@ class Operator:
     def op_list(self) -> list:
         """Operator represented as a list in the QuSpin format"""
         return self._op_list
+
+    @property
+    def jax_op_list(self) -> list:
+        """
+        Operator list with jax arrays, made easy for applying operator to fock states
+        """
+        if self._jax_op_list is None:
+            self._jax_op_list = []
+            for opstr, interaction in self.op_list:
+                J_array = []
+                index_array = []
+                for J, *index in interaction:
+                    J_array.append(J)
+                    index_array.append(index)
+                J_array = local_to_replicate(J_array).astype(get_default_dtype())
+                index_array = local_to_replicate(index_array).astype(jnp.uint16)
+                self._jax_op_list.append([opstr, J_array, index_array])
+
+        return self._jax_op_list
 
     @property
     def expression(self) -> str:
@@ -248,91 +347,6 @@ class Operator:
             return self.__imul__(1 / other)
         return NotImplemented
 
-    def apply_diag(self, fock_states: np.ndarray) -> np.ndarray:
-        r"""
-        Apply the diagonal elements of the operator
-
-        :param fock_states:
-            A batch of fock states :math:`\left| x_i \right>` with elements
-            :math:`\pm 1` that the operator acts on
-
-        :return:
-            A 1D array :math:`H_z` with :math:`H_{z,i} = \left< x_i | H | x_i \right>`
-        """
-        basis_ints = array_to_ints(fock_states)
-        dtype = get_default_dtype()
-        Hz = np.zeros(basis_ints.size, dtype)
-        basis = Identity().basis
-
-        for opstr, interaction in self.op_list:
-            if all(s in ("I", "n", "z") for s in opstr):
-                for J, *index in interaction:
-                    ME, bra, ket = basis.Op_bra_ket(
-                        opstr, index, J, dtype, basis_ints, reduce_output=False
-                    )
-                    Hz += ME
-        return Hz
-
-    def apply_off_diag(
-        self, fock_states: np.ndarray, return_basis_ints: bool = False
-    ) -> dict:
-        r"""
-        Apply the non-zero non-diagonal elements of the operator. Every input fock state
-        :math:`\left| x_i \right>` is mapped to multiple output fock states
-        :math:`\left| x'_j \right>`.
-
-        :param fock_states:
-            A 2D array as a batch of input fock states :math:`\left| x_i \right>` with
-            elements :math:`\pm 1`
-
-        :param return_basis_ints:
-            Whether to return the connected states :math:`\left| x'_j \right>` as basis
-            integers or fock states, default to fock states.
-
-        :return:
-            segment:
-                A 1D array showing how :math:`\left| x'_j \right>` is connected to
-                :math:`\left| x_i \right>`. The j'th element of this array is i.
-
-            s_conn:
-                An array as a batch of connected states :math:`\left| x'_j \right>`
-
-            H_conn:
-                A 1D array :math:`H_c` with :math:`H_{c,j} = \left< x'_j | H | x_i \right>`
-        """
-        basis = Identity().basis
-        basis_ints = array_to_ints(fock_states)
-        dtype = get_default_dtype()
-        arange = np.arange(basis_ints.size, dtype=np.uint32)
-        out = dict()
-
-        for opstr, interaction in self.op_list:
-            nflips = sum(1 for s in opstr if s not in ("I", "n", "z"))
-            if nflips > 0:
-                for J, *index in interaction:
-                    ME, bra, ket = basis.Op_bra_ket(
-                        opstr, index, J, dtype, basis_ints, reduce_output=False
-                    )
-                    is_nonzero = ~np.isclose(ME, 0.0)
-                    segment = arange[is_nonzero]
-                    x_conn = bra[is_nonzero]
-                    H_conn = ME[is_nonzero]
-
-                    if nflips in out:
-                        out[nflips][0].append(segment)
-                        out[nflips][1].append(x_conn)
-                        out[nflips][2].append(H_conn)
-                    else:
-                        out[nflips] = [[segment], [x_conn], [H_conn]]
-
-        for nflips, (segment, x_conn, H_conn) in out.items():
-            x_conn = np.concatenate(x_conn)
-            if not return_basis_ints:
-                x_conn = ints_to_array(x_conn)
-            out[nflips] = [np.concatenate(segment), x_conn, np.concatenate(H_conn)]
-
-        return out
-
     def psiOloc(
         self, state: State, samples: Union[Samples, np.ndarray, jax.Array]
     ) -> jax.Array:
@@ -342,19 +356,27 @@ class Operator:
             else:
                 internal = samples.state_internal
             x = samples.spins
-            x_local = np.asarray(global_to_local(samples.spins))
             wf = samples.wave_function
         else:
             x = to_global_array(samples)
             internal = state.init_internal(x)
             wf = state(x)
-            x_local = np.asarray(global_to_local(x))
 
-        Hz = local_to_global(self.apply_diag(x_local))
-
-        off_diags = self.apply_off_diag(x_local)
+        Hz = _apply_diag(x, self.jax_op_list)
+        off_diags = _apply_off_diag(x, self.jax_op_list)
         psiHx = jnp.zeros(x.shape[0], Hz.dtype, device=get_global_sharding())
-        for nflips, (segment, x_conn, H_conn) in off_diags.items():
+
+        for nflips, (x_conn, H_conn) in off_diags.items():
+            x_conn = np.asarray(global_to_local(x_conn))
+            H_conn = np.asarray(global_to_local(H_conn))
+            valid = ~np.isnan(H_conn)
+            idx_start = x_conn.shape[0] * jax.process_index()
+            segment = np.arange(idx_start, idx_start + x_conn.shape[0])
+            segment = np.repeat(segment[:, None], x_conn.shape[1], axis=1)
+            segment = segment[valid]
+            H_conn = H_conn[valid]
+            x_conn = x_conn[valid, :]
+
             n_conn = np.max(process_allgather(x_conn.shape[0])).item()
             if hasattr(state, "max_parallel") and state.max_parallel is not None:
                 mp = state.max_parallel * jax.local_device_count()
@@ -363,10 +385,10 @@ class Operator:
             else:
                 n_conn_extend = n_conn
             pad_width = (0, n_conn_extend - x_conn.shape[0])
-            segment += jax.process_index() * x_local.shape[0]
             segment = local_to_global(np.pad(segment, pad_width))
             H_conn = local_to_global(np.pad(H_conn, pad_width))
             x_conn = local_to_global(np.pad(x_conn, (pad_width, (0, 0))))
+
             psi_conn = state.ref_forward(
                 x_conn, x, nflips, segment, internal, update_maximum=True
             )
