10925fd812eebb3b96d48c60fac7b5135ab9bc31
ChenAo-Phys <chenao.phys@gmail.com>
2025-06-10 18:08:53 -0700

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,8 +33,7 @@ default_role = "py:obj"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = "pydata_sphinx_theme"
-html_theme = "classic"
-#html_static_path = ["../../_static"]
+html_theme = "pydata_sphinx_theme"
+#html_static_path = ["_static"]
 html_use_relative_urls = True
 html_baseurl = 'https://chenao-phys.github.io/quantax/'
