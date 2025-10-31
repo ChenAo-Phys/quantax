ee96a3ec5e34e002df6129ab508b04ea95ee5c82
Ao Chen <chenao.phys@gmail.com>
2025-10-30 21:41:06 -0700

    remove sign net and add compute_sign functions

--- quantax/model/__init__.py
+++ quantax/model/__init__.py
@@ -1,7 +1,6 @@
 from .shallow_nets import SingleDense, RBM_Dense, SingleConv, RBM_Conv
 from .conv_nets import ResConv, ResGConv
 from .transformer import ConvTransformer
-from .sign_nets import SgnNet, MarshallSign, StripeSign, Neel120
 from .triangular_nets import Triangular_Neighbor_Conv, Triangular_ResConv
 from .fermion_mf import (
     GeneralDet,
