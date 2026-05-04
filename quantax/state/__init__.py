924389736a20d12ed773ab26d9a7c04496f53885
Ao Chen <chenao.phys@gmail.com>
2026-05-04 12:06:43 -0700

    remove lanczos state

--- quantax/state/__init__.py
+++ quantax/state/__init__.py
@@ -1,6 +1,5 @@
 from .state import State, DenseState
 from .variational import VS_TYPE, Variational
-from .lanczos import StateLS
 from .fermion_mf import (
     MeanFieldFermionState,
     GeneralDetState,
