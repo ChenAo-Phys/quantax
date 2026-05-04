520f4b414b37244624d628f249659de275e07883
Ao Chen <chenao.phys@gmail.com>
2026-05-04 10:20:47 -0700

    remove transformers

--- quantax/model/__init__.py
+++ quantax/model/__init__.py
@@ -11,6 +11,5 @@ from .fermion_mf import (
     MultiPf,
     PartialPair,
 )
-from .transformer import Transformer, Embedding, MHSA, FFN
 from .jastrow import NeuralJastrow
 from .backflow import DetBackflow, PfBackflow
